c2e13d04f85addcc3fe1257de74d64a674c2ec8c
SUPERIORII <adukuly461@gmail.com>
2024-06-21 20:50:41 +0000

    music app

--- script.js
+++ script.js
@@ -7,14 +7,137 @@ const timelineContainer = document.querySelector('.timeline-container');
 const loopingBtn = document.querySelector('.looping-btn');
 const slider = document.querySelector('.volume-slider');
 
-const fowardBtn = document.querySelector('.foward-btn');
+const forwardBtn = document.querySelector('.forward-btn');
+const backWardBtn = document.querySelector('.backward-btn');
 const mutedBtn = document.querySelector('.muted-btn');
 const playbackBtn = document.querySelector('.playback-btn');
 
+const musicPicture = document.querySelector('.music-picture')
+const musicImg = document.querySelector('.music-img')
+const musicTitle = document.querySelector('.music-title')
+const musicName = document.querySelector('.music-name')
+const musicArtist = document.querySelector('.music-artist')
+const musicArtistName = document.querySelector('.music-artist-name')
+
+console.log(backWardBtn);
+
+//audio listed array
+const audioList =[
+    {
+        id: 1,
+        music: "assests/music/Dax_-__Book_Of_Revelations__(Official_Music_Video)(256k).mp3",
+        atist: "Dax",
+        music_name: "Book of Revelations",
+        image: "assests/img/img1.jpg",
+        },
+    {
+        id: 2,
+        music: "assests/music/`TEDDYRIDE_King_Solomon(360p).mp3",
+        atist: "TeddyRide",
+        music_name: "King Solomon",
+        image: "assests/img/img2.jpg",
+    },
+    {
+        id: 3,
+        music: "assests/music/Dax_-__Dear_Mom__(Official_Music_Video)(256k).mp3",
+        atist: "Dax",
+        music_name: "Dear Mom",
+        image: "assests/img/img3.png",
+    },
+    {
+        id: 4,
+        music: "assests/music/Dax_-__Depression__(Official_Music_Video)(256k).mp3",
+        atist: "Dax",
+        music_name: "Depression",
+        image: "assests/img/img4.png",
+    },
+    {
+        id: 5,
+        music: "assests/music/Dax_-__I_Can't_Breathe__(Official_Music_Video)(256k).mp3",
+        atist: "Dax",
+        music_name: "I can't Breathe",
+        image: "assests/img/img1.jpg",
+    },
+    {
+        id: 6,
+        music: "assests/music/Dax_-_My_Eyes_Bleed_(Official_Audio)(256k).mp3",
+        atist: "Dax",
+        music_name: "My Eyes Bleed",
+        image: "assests/img/img3.png",
+    },
+    {
+        id: 7,
+        music: "assests/music/MattyBRaps_-_Can’t_Get_You_Off_My_Mind(360p).mp3",
+        atist: "MattyBRaps",
+        music_name: "Can't get you off my mind",
+        image: "assests/img/img4.png",
+    },
+    {
+        id: 8,
+        music: "assests/music/Rema_Calm_Down_(Official_Music_Video)(480p).mp3",
+        atist: "Rema",
+        music_name: "Calm Down",
+        image: "assests/img/img3.png",
+    },
+    {
+        id: 9,
+        music: "assests/music/MattyBRaps_-_Here_We_Go_Again_(ft_Auti_G)(360p).mp3",
+        atist: "MattyBRaps",
+        music_name: "Here we go Again",
+        image: "assests/img/img2.jpg",
+    },
+    {
+        id: 10,
+        music: "assests/music/MattyBRaps_-_Ready_This_Time_(Lyrics)(480p).mp3",
+        atist: "MattyBRaps",
+        music_name: "Ready This Time",
+        image: "assests/img/img1.jpg",
+    },
 
 
-//implement the speed of the audio
+]
 
+audio.src = audioList[0].music
+musicPicture.src = audioList[0].image
+musicImg.src = audioList[0].image
+musicArtist.textContent = audioList[0].atist
+musicName.textContent = audioList[0].music_name
+musicArtistName.textContent = audioList[0].atist
+musicTitle.textContent = audioList[0].music_name
+
+// looping the audios in sequence 
+function loopingThroughAudio() {
+    let lastElem = audioList.pop()
+    audioList.unshift(lastElem)
+    console.log(lastElem);
+    
+    if (!audioContainer.classList.contains('repeate') && lastElem) {
+
+        audio.src = lastElem.music;
+        musicPicture.src = lastElem.image;
+        musicImg.src = lastElem.image;
+        musicArtist.textContent = lastElem.atist;
+        musicArtistName.textContent = `(prod by ${lastElem.atist})`;
+        musicTitle.textContent = lastElem.music_name;
+        musicName.textContent = lastElem.music_name;
+        audio.play()
+    }
+}
+
+//executing the loopingThroughAudio function above
+audio.addEventListener('ended', loopingThroughAudio)
+
+// fowarding audio 5 seconds 
+forwardBtn.addEventListener('click', ()=>{
+    audio.currentTime +=5
+});
+
+//backwading the audio 5 second
+backWardBtn.addEventListener('click', ()=>{
+    audio.currentTime -=5
+});
+
+//implement the playback speed of each audio
 playbackBtn.addEventListener('click', ()=>{
     let newplayBackRate = audio.playbackRate + .25;
 
@@ -30,14 +153,12 @@ playbackBtn.addEventListener('click', ()=>{
 slider.addEventListener('input', e=>{
     audio.muted = e.target.value === 0
     audio.volume = e.target.value
-
+    console.log(audio.volume);
 })
 
 //setting the volume level of input when it is move and the icon to show
 audio.addEventListener('volumechange', ()=>{
      slider.value = audio.volume
-
-    console.log(slider.value);
     let volumeLevel;
 
      if (audio.muted || audio.volume ===0) {
@@ -59,122 +180,26 @@ mutedBtn.addEventListener('click', ()=>{
 })
 
 
-//audio listed array
-const audioList =[
-    {
-        id: 1,
-        music: "assests/music/Dax_-__Book_Of_Revelations__(Official_Music_Video)(256k).mp3",
-        atist: "Dax",
-        music_name: "Book of Revelations",
-        duration: audio.duration,
-        },
-    {
-        id: 2,
-        music: "assests/music/`TEDDYRIDE_King_Solomon(360p).mp3",
-        atist: "TeddyRide",
-        music_name: "King Solomon",
-        duration: audio.duration,
-    },
-    {
-        id: 3,
-        music: "assests/music/Dax_-__Dear_Mom__(Official_Music_Video)(256k).mp3",
-        atist: "Dax",
-        music_name: "Dear Mom",
-        duration: audio.duration, 
-    },
-    {
-        id: 4,
-        music: "assests/music/Dax_-__Depression__(Official_Music_Video)(256k).mp3",
-        atist: "Dax",
-        music_name: "Depression Official",
-        duration: audio.duration,
-    },
-    {
-        id: 5,
-        music: "assests/music/Dax_-__I_Can't_Breathe__(Official_Music_Video)(256k).mp3",
-        atist: "Dax",
-        music_name: "I can't Breathe",
-        duration: audio.duration,
-    },
-]
-
-//popular music list
-const middleLeftSection = document.getElementsByClassName('middle-left-section');
-let popularMusicContainer = document.querySelector('.popular-music-container');
-
-
-/*
-
-for (let i = 0; i < audioList.length; i++) {
-    let list = audioList[i]
-    console.log(list)
-    let popularMusic = document.createElement('div');
-     popularMusic.classList.add('popular-music');
-
-     popularMusic.innerHTML=`<div class="id-icon-wrapper">
-                            <div class="id">${list.id}</div>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="play-music-icon" viewBox="0 0 16 16">
-                                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
-                            </svg>
-                        </div>
-        
-                        <div class="popular-music-name">
-        
-                            <div class="artist-name-wrapper">
-                                <p class="music-name">${list.music_name}</p>
-                                <p class="artist-name">${list.atist}</p>
-                            </div>
-                        </div>
-        
-                        <div class="audio-info">
-                            <div class="audio-duration">02:22</div>
-                        </div>`
-
-     popularMusicContainer.appendChild(popularMusic)
-     
- 
-                            
-}
-
-*/
 
 //repeating mode of the audio
-loopingBtn.addEventListener('click', toggleRepeateMode);
-
-function toggleRepeateMode() {
+loopingBtn.addEventListener('click', ()=>{
     audioContainer.classList.toggle('repeate')
-}
-
-
+});
 
 function repeate() {
-
     if (audioContainer.classList.contains('repeate')) {
-        let currentTime = audio.currentTime = 0;
-        audio.play()
-        
-    }
-    
+       audio.currentTime = 0;
+        audio.play();
+    } 
 }
 
-audio.addEventListener('ended', repeate)
+//executing the reeate function above
+audio.addEventListener('ended', repeate);
+
 
 
 //nexting the audio to the next music
-fowardBtn.addEventListener('click', nextVideo);
 
-function nextVideo() {
-    let lastElem =audioList.pop();
-    audioList.unshift(lastElem)
-    //console.log(shiftedData);
-    
-    if (lastElem) {
-        audio.src = lastElem.music;
-        audio.play()
-    }   
-   
-}
-audio.src = audioList[0].music
 
 
 
@@ -185,10 +210,7 @@ timelineContainer.addEventListener('mousedown', (e)=>{
     const rect = timelineContainer.getBoundingClientRect()
     const percent = Math.min(Math.max(0, e.x - rect.x), rect.width)/ rect.width;
     timelineContainer.style.setProperty("--preview-position", percent)
-
-    console.log(percent);
 })
-
 
 
 playPausedBtn.addEventListener('click', playPaused)
@@ -221,7 +243,6 @@ const leadingZero = new Intl.NumberFormat(undefined, {
 })
 
 
-
 function formatDuration(time) {
     const second = Math.floor(time % 60);
     const minute = Math.floor(time /60)%60;
@@ -229,7 +250,7 @@ function formatDuration(time) {
 
     if (hour === 0) {
         return `${leadingZero.format(minute)}:${leadingZero.format(second)}`
-    }else if(hour>1) {
+    }else{
         `${leadingZero.format(hour)}:${leadingZero.format(minute)}:${leadingZero.format(second)}`
     }
 }
